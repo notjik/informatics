9720165a73362b5103f8f66c37ffb969a9df4824
notjik <notjik@yandex.ru>
2022-11-07 15:01:26 +0500

    correcting mistakes

--- classworks/probny/main.py
+++ classworks/probny/main.py
@@ -74,17 +74,17 @@ def toBASE(num: int, base: int) -> str:
 ##        print(a)
 
 
-def f(n):
-    if n == 0:
-        return 3
-    if 0 < n <= 15:
-        return f(n-1)
-    if 15 < n < 95:
-        return 2.5 * f(n-3)
-    return 3.3 * f(n-2)
-
-
-print(f(70))
+##def f(n):
+##    if n == 0:
+##        return 3
+##    if 0 < n <= 15:
+##        return f(n-1)
+##    if 15 < n < 95:
+##        return 2.5 * f(n-3)
+##    return 3.3 * f(n-2)
+##
+##
+##print(f(70))
 
 
 ##with open('data/17-4.txt') as f:
