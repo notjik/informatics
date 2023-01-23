fde7695521d054d503d7039be264b91aaec7400a
notjik <notjik@yandex.ru>
2023-01-23 23:02:45 +0500

    update algorithms

--- libs/algorithms.py
+++ libs/algorithms.py
@@ -6,7 +6,7 @@ class Sort:
     # TODO: Bubble sorting method (https://sortvisualizer.com/bubblesort/)
     # TODO: Метод сортировки пузырьком (https://sortvisualizer.com/bubblesort/)
     def bubble(array: list | tuple | str,
-               alg: Callable[[int | float | str, int | float | str], list] = lambda x, y: x < y,
+               alg: Callable[[int | float | str, int | float | str], bool] = lambda x, y: x < y,
                reverse: bool = False):
         tp = type(array)
         if tp != list:
@@ -15,15 +15,13 @@ class Sort:
             for j in range(len(array) - i - 1):
                 if alg(array[j + 1], array[j]) if not reverse else alg(array[j], array[j + 1]):
                     array[j + 1], array[j] = array[j], array[j + 1]
-        if tp == str:
-            return ''.join(array)
-        return tp(array)
+        return ''.join(array) if tp == str else tp(array)
 
     @staticmethod
     # TODO: Shaker sorting method (https://sortvisualizer.com/shakersort/)
     # TODO: Метод сортировки шейкером (https://sortvisualizer.com/shakersort/)
     def shaker(array: list | tuple | str,
-               alg: Callable[[int | float | str, int | float | str], list] = lambda x, y: x < y,
+               alg: Callable[[int | float | str, int | float | str], bool] = lambda x, y: x < y,
                reverse: bool = False):
         tp = type(array)
         if tp != list:
@@ -38,15 +36,13 @@ class Sort:
                 if alg(array[i + 1], array[i]) if not reverse else alg(array[i], array[i + 1]):
                     array[i], array[i + 1] = array[i + 1], array[i]
             right -= 1
-        if tp == str:
-            return ''.join(array)
-        return tp(array)
+        return ''.join(array) if tp == str else tp(array)
 
     @staticmethod
     # TODO: Insertion sorting method (https://sortvisualizer.com/shakersort/)
     # TODO: Метод сортировки вставками (https://sortvisualizer.com/shakersort/)
     def insertion(array: list | tuple | str,
-                  alg: Callable[[int | float | str, int | float | str], list] = lambda x, y: x < y,
+                  alg: Callable[[int | float | str, int | float | str], bool] = lambda x, y: x < y,
                   reverse: bool = False):
         tp = type(array)
         if tp != list:
@@ -58,12 +54,27 @@ class Sort:
                 array[j + 1] = array[j]
                 j -= 1
             array[j + 1] = x
-        if tp == str:
-            return ''.join(array)
-        return tp(array)
+        return ''.join(array) if tp == str else tp(array)
 
     @staticmethod
-    # TODO: Counting sorting method (https://www.cs.usfca.edu /~galles/visualization/CountingSort.html )
+    # TODO: Selection sorting method (https://sortvisualizer.com/selectionsort/)
+    # TODO: Метод сортировки выборкой (https://sortvisualizer.com/selectionsort/)
+    def selection(array: list | tuple | str,
+                  alg: Callable[[int | float | str, int | float | str], bool] = lambda x, y: x < y,
+                  reverse: bool = False):
+        tp = type(array)
+        if tp != list:
+            array = list(array)
+        for i in range(len(array)):
+            tmp = i
+            for j in range(i + 1, len(array)):
+                if alg(array[j], array[tmp]) if not reverse else alg(array[tmp], array[j]):
+                    tmp = j
+            array[i], array[tmp] = array[tmp], array[i]
+        return ''.join(array) if tp == str else tp(array)
+
+    @staticmethod
+    # TODO: Counting sorting method (https://www.cs.usfca.edu/~galles/visualization/CountingSort.html)
     # TODO: Метод сортировки подсчётом (https://www.cs.usfca.edu/~galles/visualization/CountingSort.html)
     def counting(array: list | tuple,
                  reverse: bool = False):
@@ -76,18 +87,22 @@ class Sort:
             if array_init[i]:
                 for k in range(array_init[i]):
                     result.append(i)
-        if tp == str:
-            return ''.join(array)
         return tp(result)
 
     @staticmethod
     # TODO: Merge sorting method (https://sortvisualizer.com/mergesort/)
     # TODO: Метод сортировки слиянием (https://sortvisualizer.com/mergesort/)
-    def merge(array):
+    def merge(array: list | tuple | str,
+              reverse: bool = False):
+        tp = type(array)
+        if tp != list:
+            array = list(array)
         if len(array) <= 1:
             mediana = len(array) // 2
-            left = array[:mediana]
-            right = array[mediana:]
+            if not reverse:
+                left, right = array[:mediana], array[mediana:]
+            else:
+                left, right = array[mediana:], array[:mediana]
             Sort.merge(left)
             Sort.merge(right)
             i = j = k = 0
@@ -107,7 +122,7 @@ class Sort:
                 array[k] = right[j]
                 j += 1
                 k += 1
-        return array
+        return ''.join(array) if tp == str else tp(array)
 
     @staticmethod
     # TODO: Hoare's sorting method (https://sortvisualizer.com/quicksort/)
@@ -115,9 +130,11 @@ class Sort:
     def quick(array: list | tuple | str,
               mni: int = 0,
               mxi: int | None = None,
-              alg: Callable[[int | float | str, int | float | str], list] = lambda x, y: x < y,
+              alg: Callable[[int | float | str, int | float | str], bool] = lambda x, y: x < y,
               reverse: bool = False):
-        def partition(array, mni, mxi):
+        def partition(array: list,
+                      mni: int,
+                      mxi: int):
             pivot = mni
             for i in range(mni + 1, mxi + 1):
                 if alg(array[i], array[mni]) if not reverse else alg(array[mni], array[i]):
@@ -136,19 +153,17 @@ class Sort:
         pivot = partition(array, mni, mxi)
         Sort.quick(array=array, mni=mni, mxi=pivot - 1, alg=alg, reverse=reverse)
         Sort.quick(array=array, mni=pivot + 1, mxi=mxi, alg=alg, reverse=reverse)
-        if tp == str:
-            return ''.join(array)
-        return tp(array)
+        return ''.join(array) if tp == str else tp(array)
 
 
 if __name__ == '__main__':
     from timeit import timeit
     from random import randint, choices
 
-    l = 100
+    l = 10
     array = [randint(0, 1000) for r in range(l)]
     # array = ''.join(choices(''.join(chr(i) for i in range(65, 91)), k=l))
-    operations = 100000
+    operations = 100
 
     print('Bubble:')
     print('Before: {}'.format(array))
@@ -156,12 +171,14 @@ if __name__ == '__main__':
     print('{} operations per {}\n'.format(operations, timeit('Sort.bubble({})'.format(array[:]) if type(array) != str
                                                              else 'Sort.bubble("{}")'.format(array[:]),
                                                              'from __main__ import Sort', number=operations)))
+
     print('Shaker:')
     print('Before: {}'.format(array))
     print('After: {}'.format(Sort.shaker(array[:])))
     print('{} operations per {}\n'.format(operations, timeit('Sort.shaker({})'.format(array[:]) if type(array) != str
                                                              else 'Sort.shaker("{}")'.format(array[:]),
                                                              'from __main__ import Sort', number=operations)))
+
     print('Insertion:')
     print('Before: {}'.format(array))
     print('After: {}'.format(Sort.insertion(array[:])))
@@ -169,10 +186,17 @@ if __name__ == '__main__':
                                                              else 'Sort.insertion("{}")'.format(array[:]),
                                                              'from __main__ import Sort', number=operations)))
 
+    print('Selection:')
+    print('Before: {}'.format(array))
+    print('After: {}'.format(Sort.selection(array[:])))
+    print('{} operations per {}\n'.format(operations, timeit('Sort.selection({})'.format(array[:]) if type(array) != str
+                                                             else 'Sort.selection("{}")'.format(array[:]),
+                                                             'from __main__ import Sort', number=operations)))
+
     if type(array) != str:
         print('Counting:')
         print('Before: {}'.format(array))
-        print('After: {}'.format(Sort.counting(array[:], reverse=True)))
+        print('After: {}'.format(Sort.counting(array[:])))
         print('{} operations per {}\n'.format(operations, timeit('Sort.counting({})'.format(array[:]),
                                                                  'from __main__ import Sort', number=operations)))
 
